06a29059ebb68eedaafc205ec8e86092bed10585
taink <taink317fwork@gmail.com>
2024-08-13 15:20:58 +0700

    update iot_op set state

--- internal/domain/repo/rp_state.go
+++ internal/domain/repo/rp_state.go
@@ -5,11 +5,14 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log"
 	"time"
 
+	"github.com/Dcarbon/go-shared/edef"
 	"github.com/Dcarbon/go-shared/gutils"
 	"github.com/Dcarbon/go-shared/svc"
 	"github.com/Dcarbon/iot-op/internal/domain"
+	"github.com/Dcarbon/iot-op/internal/domain/rss"
 	"github.com/Dcarbon/iot-op/internal/models"
 	"github.com/go-redis/redis/v8"
 )
@@ -19,12 +22,14 @@ var stateDuration = 500 * time.Second
 type StateImpl struct {
 	rClient *redis.Client
 	iot     svc.IIotInfo
+	pusher  *edef.NotificationEvent
 }
 
 func NewStateImpl(rClient *redis.Client, iiot svc.IIotInfo) (*StateImpl, error) {
 	var impl = &StateImpl{
 		rClient: rClient,
 		iot:     iiot,
+		pusher:  edef.NewNotificationEvent(rss.GetRabbitPusher()),
 	}
 	return impl, nil
 }
@@ -62,6 +67,22 @@ func (impl *StateImpl) Update(req *domain.RStateUpdate,
 		return err
 	}
 	// log.Println("Ok ? ", ok)
+	go func() {
+		log.Println("Publish notification maintainance.")
+		if len(state.Sensors) <= 0 {
+			return
+		}
+		runtime := state.Sensors[0].Metric["runtime"]
+		if runtime == 0 {
+			return
+		}
+		// if runtime == 500 {
+		// }
+		impl.pusher.PushNotification(&edef.EventPushNotification{
+			ProfileId: "77670e7e-3639-4411-9262-de297d984703",
+		})
+
+	}()
 
 	return nil
 }
